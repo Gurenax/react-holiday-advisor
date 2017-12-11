44ac3b5f273ca025e1b32c7522fef105142445ab
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-11 22:13:24 +1100

    Rename close button

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,7 @@ const ReviewList = ({ reviews, onClickWriteReview, formReviewActive }) => (
   <div>
     {reviews.map(review => <ReviewItem key={review._id} review={review} />)}
     <button className="btn btn-primary btn-block mt-1 mb-1" onClick={onClickWriteReview}>
-      {!formReviewActive ? 'Write a Review' : 'Close Review Form'}
+      {!formReviewActive ? 'Write a Review' : 'Close'}
     </button>
   </div>
 )
